eb9c9645f7c3bc9560572e32ec1b4d3bd171308e
MRlorenzo <964878800@qq.com>
2020-06-22 14:14:12 +0700

    优化项目。

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,55 +1,16 @@
 <template>
   <div>
     首页
+
+    <angle-eye></angle-eye>
   </div>
 </template>
 
 <script>
-
-  import AngleEyeHelper from "../port/AngleEyeHelper";
-
+  import AngleEye from './components/AngleEye';
   export default {
-    name: 'app',
-    methods: {
-    	initHooks( helper ){
-    		helper.setHooks({
-                boot( d ){
-                	console.log('天使靴开机', d.getData());
-                },
-				standBy( d ){
-                	console.log('待机')
-                },
-				cardDrawing( d ){
-                	console.log(d.getData())
-                },
-				systemError(d){},
-				gameResult(d){
-
-                },
-				cardDrawingEgig(d){},
-				cancellationOfError(d){},
-				lockOperation(d){},
-				changeOfPresetValue(d){},
-				cardDrawingRetransmission(d){
-
-                },
-				dealCardsShow(d){
-
-                },
-				revokeMultipleCards(d){},
-				default(d){}
-            });
-        }
-    },
-    created(){
-        this.$electron.ipcRenderer.send('getConfig');
-        this.$electron.ipcRenderer.on('getConfig' , (event , config)=>{
-			const { comConfig, angleConfig} = config;
-			let helper = new AngleEyeHelper(comConfig , angleConfig);
-			this.initHooks(helper);
-			window.helper = helper;
-        })
-    }
+      name: 'app',
+      components: { AngleEye }
   }
 </script>
 
---
+++ src/renderer/components/AngleEye.vue
@@ -0,0 +1,55 @@
+<template>
+    
+</template>
+
+<script>
+	import AngleEyeHelper from "../../port/AngleEyeHelper";
+
+	export default {
+		name: "angle-eye",
+		methods: {
+			initHooks( helper ){
+				helper.setHooks({
+					boot( d ){
+						console.log('天使靴开机', d.getData());
+					},
+					standBy( d ){
+						console.log('待机')
+					},
+					cardDrawing( d ){
+						console.log(d.getData())
+					},
+					systemError(d){},
+					gameResult(d){
+
+					},
+					cardDrawingEgig(d){},
+					cancellationOfError(d){},
+					lockOperation(d){},
+					changeOfPresetValue(d){},
+					cardDrawingRetransmission(d){
+
+					},
+					dealCardsShow(d){
+
+					},
+					revokeMultipleCards(d){},
+					default(d){}
+				});
+			}
+		},
+		created(){
+			this.$electron.ipcRenderer.send('getConfig');
+			this.$electron.ipcRenderer.on('getConfig' , (event , config)=>{
+				const { comConfig, angleConfig} = config;
+				let helper = new AngleEyeHelper(comConfig , angleConfig);
+				this.initHooks(helper);
+				window.helper = helper;
+			})
+		}
+	}
+</script>
+
+<style scoped>
+
+</style>

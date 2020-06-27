53e737c2add06729164c77ef5989d6e89a3e27cf
MRlorenzo <964878800@qq.com>
2020-06-27 14:20:19 +0700

    串口设置界面。

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,41 +1,48 @@
 <template>
-  <div id="main-box">
-    首页
+    <div id="main-box">
+        首页
 
-    <angle-eye @result="angleEyeResult"></angle-eye>
+        <angle-eye @result="angleEyeResult"></angle-eye>
 
-  </div>
+        <com-setting-page ref="comSetting"></com-setting-page>
+    </div>
 </template>
 
 <script>
-    import AngleEye from './components/AngleEye';
+	import AngleEye from './components/AngleEye';
+	import ComSettingPage from './components/ComSettingPage';
 	import Mousetrap from 'mousetrap';
-    export default {
-        name: 'app',
-        components: {AngleEye},
-        methods: {
-        	angleEyeResult( baccaratResult ){
-        		console.log('天使靴解析了百家乐结果:' ,  baccaratResult);
-        		this.$road.push(baccaratResult);
+
+	export default {
+		name: 'app',
+		components: {AngleEye, ComSettingPage},
+		methods: {
+			angleEyeResult(baccaratResult) {
+				console.log('天使靴解析了百家乐结果:', baccaratResult);
+				this.$road.push(baccaratResult);
+			},
+            openComSetting(){
+				this.$refs.comSetting.open();
             }
-        },
-        mounted(){
+		},
+		mounted() {
 			/*Mousetrap.bind(['f12', 'ctrl+shift+i'], ()=>{
 				this.$electron.ipcRenderer.send('openDevTools');
 			});*/
 
-			Mousetrap.bind('0 0 enter', ()=> {
+			Mousetrap.bind('0 0 enter', () => {
 				this.$electron.ipcRenderer.send('exitSystem');
-            })
+			})
 
-			Mousetrap.bind('. enter', ()=> {
-				console.log('打开com口配置')
-            })
+			Mousetrap.bind('. enter', () => {
+				this.openComSetting();
+			})
 
-        }
-    }
+		}
+	}
 </script>
 
 <style>
-  #main-box{}
+    #main-box {
+    }
 </style>

cc118fbe2f98c84622e27b686bd1846008b1856e
Lorenzo <964878800@qq.com>
2020-06-27 17:30:48 +0700

    调整

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,48 +1,82 @@
 <template>
-    <div id="main-box">
-        首页
+  <div id="main-box">
+    首页
 
-        <angle-eye @result="angleEyeResult"></angle-eye>
+    <angle-eye @result="angleEyeResult"></angle-eye>
 
-        <com-setting-page ref="comSetting"></com-setting-page>
-    </div>
+    <com-setting-page ref="comSetting" @update="connectCom"></com-setting-page>
+  </div>
 </template>
 
 <script>
-	import AngleEye from './components/AngleEye';
-	import ComSettingPage from './components/ComSettingPage';
-	import Mousetrap from 'mousetrap';
+    import AngleEye from './components/AngleEye';
+    import ComSettingPage from './components/ComSettingPage';
+    import Mousetrap from 'mousetrap';
 
-	export default {
-		name: 'app',
-		components: {AngleEye, ComSettingPage},
-		methods: {
-			angleEyeResult(baccaratResult) {
-				console.log('天使靴解析了百家乐结果:', baccaratResult);
-				this.$road.push(baccaratResult);
-			},
-            openComSetting(){
-				this.$refs.comSetting.open();
+    import ReOpenException from "../exception/ReOpenException";
+    import UnknownException from "../exception/UnknownException";
+    import UnableCloseException from "../exception/UnableCloseException";
+    import ModuleException from "../exception/ModuleException";
+
+    export default {
+        name: 'app',
+        components: {AngleEye, ComSettingPage},
+        methods: {
+            angleEyeResult(baccaratResult) {
+                console.log('天使靴解析了百家乐结果:', baccaratResult);
+                this.$road.push(baccaratResult);
+            },
+            openComSetting() {
+                this.$refs.comSetting.open();
+            },
+            closeComSetting(){
+                this.$refs.comSetting.close();
+            },
+            async connectCom( comName ){
+                const helper = this.$angleEye;
+                if (helper != null) {
+                    try {
+                        await helper.updateComName(comName);
+                        this.closeComSetting();
+                        console.log('打开成功')
+                    } catch (e) {
+                        // UnableCloseException,ModuleException,ReOpenException,UnknownException
+                        if (e instanceof ReOpenException) {
+                            console.log('重复打开')
+                        }
+                        if (e instanceof UnknownException) {
+                            console.log(e.message)
+                        }
+                        if (e instanceof UnableCloseException) {
+                            console.log('无法关闭资源')
+                        }
+                        if (e instanceof ModuleException) {
+                            console.log(e.message);
+                        }
+                    }
+                }else {
+                    this.closeComSetting();
+                }
             }
-		},
-		mounted() {
-			/*Mousetrap.bind(['f12', 'ctrl+shift+i'], ()=>{
-				this.$electron.ipcRenderer.send('openDevTools');
-			});*/
+        },
+        mounted() {
+            /*Mousetrap.bind(['f12', 'ctrl+shift+i'], ()=>{
+              this.$electron.ipcRenderer.send('openDevTools');
+            });*/
 
-			Mousetrap.bind('0 0 enter', () => {
-				this.$electron.ipcRenderer.send('exitSystem');
-			})
+            Mousetrap.bind('0 0 enter', () => {
+                this.$electron.ipcRenderer.send('exitSystem');
+            })
 
-			Mousetrap.bind('. enter', () => {
-				this.openComSetting();
-			})
+            Mousetrap.bind('. enter', () => {
+                this.openComSetting();
+            })
 
-		}
-	}
+        }
+    }
 </script>
 
 <style>
-    #main-box {
-    }
+  #main-box {
+  }
 </style>
--- src/renderer/components/ComSettingPage.vue
+++ src/renderer/components/ComSettingPage.vue
@@ -1,134 +1,113 @@
 <template>
-    <el-dialog
-            :show-close="false"
-            :close-on-press-escape="false"
-            :close-on-click-modal="false"
-            title="串口设置"
-            width="50%"
-            top="50px"
-            :center="true"
-            :visible.sync="visible"
-    >
+  <el-dialog
+          :show-close="false"
+          :close-on-press-escape="false"
+          :close-on-click-modal="false"
+          title="串口设置"
+          width="50%"
+          top="50px"
+          :center="true"
+          :visible.sync="visible"
+  >
 
-        <!--表单-->
-        <el-form :inline="false" label-width="100px">
+    <!--表单-->
+    <el-form :inline="false" label-width="100px">
 
-            <el-form-item label="串口列表">
-                <el-select v-model="comName" placeholder="请选择串口" style="width: 90%;float: left;">
-                    <el-option v-for="comName in comNames" :label="comName" :value="comName"></el-option>
-                </el-select>
-                <el-button class="el-icon-refresh" @click="refresh" style="float: right"></el-button>
-            </el-form-item>
+      <el-form-item label="串口列表">
+        <el-select v-model="comName" placeholder="请选择串口" style="width: 90%;float: left;">
+          <el-option v-for="comName in comNames" :label="comName" :value="comName"></el-option>
+        </el-select>
+        <el-button class="el-icon-refresh" @click="refresh" style="float: right"></el-button>
+      </el-form-item>
 
-            <el-form-item label="波特率">
-                <el-input v-model="comConfig.baudRate" :disabled="true" autocomplete="off"
-                          suffix-icon="el-icon-edit"></el-input>
-                <!--<el-select v-model="comConfig.baudRate" placeholder="请选择" style="width: 100%">
-                    <el-option label="1200" :value="1200"></el-option>
-                    <el-option label="2400" :value="2400"></el-option>
-                    <el-option label="4800" :value="4800"></el-option>
-                    <el-option label="9600" :value="9600"></el-option>
-                </el-select>-->
+      <el-form-item label="波特率">
+        <el-input v-model="comConfig.baudRate" :disabled="true" autocomplete="off"
+                  suffix-icon="el-icon-edit"></el-input>
+        <!--<el-select v-model="comConfig.baudRate" placeholder="请选择" style="width: 100%">
+            <el-option label="1200" :value="1200"></el-option>
+            <el-option label="2400" :value="2400"></el-option>
+            <el-option label="4800" :value="4800"></el-option>
+            <el-option label="9600" :value="9600"></el-option>
+        </el-select>-->
 
-            </el-form-item>
+      </el-form-item>
 
-            <el-form-item label="数据位">
-                <el-input v-model="comConfig.dataBits" :disabled="true" autocomplete="off"
-                          suffix-icon="el-icon-edit"></el-input>
-            </el-form-item>
+      <el-form-item label="数据位">
+        <el-input v-model="comConfig.dataBits" :disabled="true" autocomplete="off"
+                  suffix-icon="el-icon-edit"></el-input>
+      </el-form-item>
 
-            <el-form-item label="校验位">
-                <el-input v-model="comConfig.parity" :disabled="true" autocomplete="off"
-                          suffix-icon="el-icon-edit"></el-input>
-                <!--<el-select v-model="comConfig.parity" placeholder="请选择" style="width: 100%">
-                    <el-option label="none" value="none"></el-option>
-                    <el-option label="odd" value="odd"></el-option>
-                    <el-option label="even" value="even"></el-option>
-                    <el-option label="mark" value="mark"></el-option>
-                    <el-option label="space" value="space"></el-option>
-                </el-select>-->
-            </el-form-item>
+      <el-form-item label="校验位">
+        <el-input v-model="comConfig.parity" :disabled="true" autocomplete="off"
+                  suffix-icon="el-icon-edit"></el-input>
+        <!--<el-select v-model="comConfig.parity" placeholder="请选择" style="width: 100%">
+            <el-option label="none" value="none"></el-option>
+            <el-option label="odd" value="odd"></el-option>
+            <el-option label="even" value="even"></el-option>
+            <el-option label="mark" value="mark"></el-option>
+            <el-option label="space" value="space"></el-option>
+        </el-select>-->
+      </el-form-item>
 
-            <el-form-item label="停止位">
-                <el-input v-model="comConfig.stopBits" :disabled="true" autocomplete="off"
-                          suffix-icon="el-icon-edit"></el-input>
-            </el-form-item>
+      <el-form-item label="停止位">
+        <el-input v-model="comConfig.stopBits" :disabled="true" autocomplete="off"
+                  suffix-icon="el-icon-edit"></el-input>
+      </el-form-item>
 
-        </el-form>
+    </el-form>
 
-        <!--表单控制区-->
-        <div slot="footer" class="dialog-footer">
-            <el-button @click="close">取消</el-button>
-            <el-button type="primary" @click="submit">连接</el-button>
-        </div>
+    <!--表单控制区-->
+    <div slot="footer" class="dialog-footer">
+      <el-button @click="close">取消</el-button>
+      <el-button type="primary" @click="submit">连接</el-button>
+    </div>
 
-    </el-dialog>
+  </el-dialog>
 </template>
 
 <script>
-	import {getComNameList} from "../../port/utils";
-	import ReOpenException from "../../exception/ReOpenException";
-	import UnknownException from "../../exception/UnknownException";
-	import UnableCloseException from "../../exception/UnableCloseException";
-	import ModuleException from "../../exception/ModuleException";
+    import {getComNameList} from "../../port/utils";
 
-	export default {
-		name: "com-setting-page",
-		data() {
-			return {
-				comNames: [],
-				comName: null,
-				comConfig: {
-					baudRate: 4800,
-					dataBits: null,
-					parity: 'none',
-					stopBits: null
+    export default {
+        name: "com-setting-page",
+        data() {
+            return {
+                comNames: [],
+                comName: null,
+                comConfig: {
+                    baudRate: 4800,
+                    dataBits: null,
+                    parity: 'none',
+                    stopBits: null
                 },
-				visible: false
-			}
-		},
-		methods: {
-			open() {
-				this.visible = true;
-			},
-			close() {
-				this.visible = false;
-			},
-			/*连接*/
-			async submit() {
-				const helper = this.$angleEye;
-				if (helper != null) {
-                    try {
-                    	await helper.updateComName(this.comName);
-						this.visible = false;
-                    	console.log('打开成功')
-                    }catch (e){
-                        // UnableCloseException,ModuleException,ReOpenException,UnknownException
-                        if (e instanceof ReOpenException){
-                        	console.log('重复打开')
-                        }
-                        if (e instanceof UnknownException){
-                        	console.log(e.message)
-                        }
-                        if (e instanceof UnableCloseException){
-                        	console.log('无法关闭资源')
-                        }
-                        if (e instanceof ModuleException){
-                        	console.log(e.message);
-                        }
-                    }
-				}
-			},
-			async refresh() {
-				this.comNames = await getComNameList();
-			}
-		},
-		created() {
-			this.refresh().catch(e => {
-				console.error(e);
-			})
-		}
-	}
+                visible: false
+            }
+        },
+        methods: {
+            open() {
+                this.visible = true;
+            },
+            close() {
+                this.visible = false;
+            },
+            /*连接*/
+            async submit() {
+                if (this.comName != null){
+                    this.$emit('update' , this.comName)
+                }else{
+                    this.close();
+                }
+            },
+            async refresh() {
+                this.comNames = await getComNameList();
+            }
+        },
+        created() {
+            this.refresh().catch(e => {
+                console.error(e);
+            })
+        }
+    }
 </script>
 
 <style scoped>
